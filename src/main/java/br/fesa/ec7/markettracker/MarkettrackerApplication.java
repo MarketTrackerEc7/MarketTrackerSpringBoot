4784d3f6da9a475eba2c9ad47293ac46cb5854a5
Johnny Messias Paulino <johnnymessiaspaulino@gmail.com>
2022-09-04 17:52:05 -0300

    inicio com spring boot

---
+++ src/main/java/br/fesa/ec7/markettracker/MarkettrackerApplication.java
@@ -0,0 +1,13 @@
+package br.fesa.ec7.markettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkettrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkettrackerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/fesa/ec7/markettracker/models/Produto.java
@@ -0,0 +1,28 @@
+package br.fesa.ec7.markettracker.models;
+
+public class Produto {
+	
+	private int id;
+	private String descricao;
+	private String fabricante;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+}
