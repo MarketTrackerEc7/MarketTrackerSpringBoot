7dfa122028422de9b9d6e736f0677fe8c2ad2a54
market tracker <markettrackerec07@gmail.com>
2022-09-07 02:04:10 -0300

    adequacoes html p facilitar no spring boot

---
+++ src/main/java/br/fesa/ec7/markettracker/controllers/UsuarioController.java
@@ -0,0 +1,14 @@
+package br.fesa.ec7.markettracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UsuarioController {
+	
+	@RequestMapping("/cadastrarUsuario")
+	public String form() {
+		return "formUsuario";
+	}
+
+}
