cf4553a74bdb944474ae91c044b72b8897bc39b4
Johnny Messias Paulino <90730926+JohnnyMessiasPaulino@users.noreply.github.com>
2022-09-18 10:00:04 -0300

    Implementado pesquisa produtos da API tela
    Implementado pesquisa produtos da API tela

---
+++ src/main/java/br/fesa/ec7/markettracker/models/Texto.java
@@ -0,0 +1,15 @@
+package br.fesa.ec7.markettracker.models;
+
+public class Texto {
+	
+	private String texto;
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+}
